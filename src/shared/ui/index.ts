2d139ddfb7f4c48382140658422256e2fb7afe63
NastyaZ23 <nastyh1233@gmail.com>
2023-04-27 18:52:34 +0300

    feat: add updateButton

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,5 @@
 export { Header } from './Header/Header';
+export { UpdateButton } from './UpdateButton/UpdateButton';
 export { ControlledRating } from './ControlledRating/ControlledRating';
 export { CustomList } from './CustomList/CustomList';
 export { CustomListItem } from './CustomListItem/CustomListItem';
